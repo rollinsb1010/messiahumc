f65dc8f03ed71d8787507b68fcc7b66d44a3c26f
Mike Doel <mike@mikedoel.com>
2011-05-28 12:51:08 -0400

    trying to get s3

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Messiahumc
   class Application < Rails::Application
-    config.middleware.insert 0, 'Dragonfly::Middleware', :images
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/initializers/dragonfly.rb
+++
@@ -1,8 +0,0 @@
-require 'dragonfly'
-app = Dragonfly[:images]
-
-app.configure_with(:imagemagick)
-app.configure_with(:rails)
-app.configure_with(:heroku, 'messiahumc-production') if Rails.env.production?
-
-app.define_macro(ActiveRecord::Base, :image_accessor)
