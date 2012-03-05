2976cc06392792a7c81ed656630a1349e554334c
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-05 16:48:40 -0400

    Adding assets, and some views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def event_schedule(event)
+    if event.single_day?
+      "#{event.start_at.strftime('%B %e, %l:%M %p')} to #{event.end_at.strftime('%l:%M %p')}"
+    else
+      "#{event.start_at.strftime('%B %e')} to #{event.end_at.strftime('%B %e')}"
+    end
+  end
 end
