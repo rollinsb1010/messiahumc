f0483f9d9e4899192eb0abb5929f7d4151cb76ff
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-05 17:47:58 -0400

    Re-create pastors engine to set image fields correctly

--- vendor/extensions/pastors/app/models/refinery/pastors/pastor.rb
+++ vendor/extensions/pastors/app/models/refinery/pastors/pastor.rb
@@ -6,7 +6,11 @@ module Refinery
       acts_as_indexed :fields => [:name, :job_title, :bio, :email]
 
       validates :name, :presence => true, :uniqueness => true
-              
+          
+      belongs_to :thumbnail, :class_name => '::Refinery::Image'
+    
+      belongs_to :large_photo, :class_name => '::Refinery::Image'
+        
     end
   end
 end
