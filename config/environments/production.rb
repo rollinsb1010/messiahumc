58ed14fa840763033e9b2502e6d2f57fe142af08
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-03-06 21:07:39 -0400

    Added fog and s3_backend.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,5 @@ MessiahUmc::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  ::Refinery::Core.config.s3_backend = true
 end
