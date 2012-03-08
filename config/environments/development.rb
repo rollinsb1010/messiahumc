a2f6cc488c35883c6a242ed4697447b711e19314
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-03-08 17:13:17 -0400

    Fixed Procfile for foreman.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,9 @@ MessiahUmc::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation = :notify
+
   # Do not compress assets
   config.assets.compress = false
 
