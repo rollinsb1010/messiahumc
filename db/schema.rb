51e6930ec571f2220686b6fb2542e263e7bff45e
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-12 19:49:36 -0400

    Adding sections and default values, still needs to be formatted correctly

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120308023821) do
+ActiveRecord::Schema.define(:version => 20120312222351) do
 
   create_table "refinery_copywriting_phrase_translations", :force => true do |t|
     t.integer  "refinery_copywriting_phrase_id"
