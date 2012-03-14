d9c2f67532ea4ab4ff07348bdbf1096286dee84d
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-14 15:06:53 -0400

    Placed StaffCategories controller on correct location

--- vendor/extensions/staff_members/app/controllers/refinery/staff_members/admin/staff_categories_controller.rb
+++ vendor/extensions/staff_members/app/controllers/refinery/staff_members/admin/staff_categories_controller.rb
@@ -3,8 +3,7 @@ module Refinery
     module Admin
       class StaffCategoriesController < ::Refinery::AdminController
 
-        crudify :'refinery/staff_members/staff_category',
-                :title_attribute => 'name', :xhr_paging => true
+        crudify :'refinery/staff_members/staff_category', :title_attribute => 'name', :xhr_paging => true
 
       end
     end
