cd136849a45a0e9cd9e1dfcd9ef11eadc31a69f8
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-14 17:51:30 -0400

    Added email field regex validation for Staff Members and  made before filter for categories to always run

--- vendor/extensions/staff_members/app/controllers/refinery/staff_members/admin/staff_members_controller.rb
+++ vendor/extensions/staff_members/app/controllers/refinery/staff_members/admin/staff_members_controller.rb
@@ -2,7 +2,7 @@ module Refinery
   module StaffMembers
     module Admin
       class StaffMembersController < ::Refinery::AdminController
-        before_filter :get_staff_categories, only: [:new, :edit]
+        before_filter :get_staff_categories
 
         crudify :'refinery/staff_members/staff_member', title_attribute: 'name', xhr_paging: true
 
--- vendor/extensions/staff_members/app/models/refinery/staff_members/staff_member.rb
+++ vendor/extensions/staff_members/app/models/refinery/staff_members/staff_member.rb
@@ -6,9 +6,10 @@ module Refinery
       acts_as_indexed fields: [:name, :job_title, :bio, :email]
 
       validates :name, presence: true, uniqueness: true
+      validates_format_of :email, with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/i
 
       belongs_to :photo, class_name: '::Refinery::Image'
-      
+
       belongs_to :category
 
     end
