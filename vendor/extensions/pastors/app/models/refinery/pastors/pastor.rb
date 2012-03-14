fe454b27abf6f1f8ca400023dd69f0cf32e0eb95
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-14 18:03:24 -0400

    Adding deafault sorting by position ASC to pastors and sermons as well

--- vendor/extensions/pastors/app/models/refinery/pastors/pastor.rb
+++ vendor/extensions/pastors/app/models/refinery/pastors/pastor.rb
@@ -1,6 +1,8 @@
 module Refinery
   module Pastors
     class Pastor < Refinery::Core::BaseModel
+      default_scope order: 'position ASC'
+
       self.table_name = 'refinery_pastors'
 
       acts_as_indexed fields: [:name, :job_title, :bio, :email]
