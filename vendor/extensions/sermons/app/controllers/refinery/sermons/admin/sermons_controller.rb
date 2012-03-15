f9b17e778ec3d9abdc33c28b213d1fbe3bc419d5
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-15 15:13:46 -0400

    Adding relationship between sermon and pastors

--- vendor/extensions/sermons/app/controllers/refinery/sermons/admin/sermons_controller.rb
+++ vendor/extensions/sermons/app/controllers/refinery/sermons/admin/sermons_controller.rb
@@ -2,9 +2,13 @@ module Refinery
   module Sermons
     module Admin
       class SermonsController < ::Refinery::AdminController
+        before_filter :get_pastors
 
-        crudify :'refinery/sermons/sermon', :xhr_paging => true
+        crudify :'refinery/sermons/sermon', xhr_paging: true
 
+        def get_pastors
+          @pastors = ::Refinery::Pastors::Pastor.all
+        end
       end
     end
   end
--- vendor/extensions/sermons/app/models/refinery/sermons/sermon.rb
+++ vendor/extensions/sermons/app/models/refinery/sermons/sermon.rb
@@ -2,16 +2,16 @@ module Refinery
   module Sermons
     class Sermon < Refinery::Core::BaseModel
       default_scope order: 'position ASC'
-      self.table_name = 'refinery_sermons'      
-    
-      acts_as_indexed :fields => [:title, :location, :description, :scripture_reading]
+      self.table_name = 'refinery_sermons'
 
-      validates :title, :presence => true, :uniqueness => true
-          
-      belongs_to :image, :class_name => '::Refinery::Image'
-        
-      belongs_to :mp3_file, :class_name => '::Refinery::Resource'
-    
+      acts_as_indexed fields: [:title, :location, :description, :scripture_reading]
+
+      validates :title, presence: true, uniqueness: true
+
+      belongs_to :image, class_name: '::Refinery::Image'
+      belongs_to :mp3_file, class_name: '::Refinery::Resource'
+      belongs_to :pastor, class_name: '::Refinery::Pastors::Pastor'
+
     end
   end
 end
