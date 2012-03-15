3af965a0e241e3d044980902c162ccd434d87537
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-15 14:24:52 -0400

    Remove old sermons engine

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120314224242) do
+ActiveRecord::Schema.define(:version => 20120315182218) do
 
   create_table "refinery_copywriting_phrase_translations", :force => true do |t|
     t.integer  "refinery_copywriting_phrase_id"
@@ -204,16 +204,6 @@ ActiveRecord::Schema.define(:version => 20120314224242) do
 
   add_index "refinery_roles_users", ["role_id", "user_id"], :name => "index_refinery_roles_users_on_role_id_and_user_id"
   add_index "refinery_roles_users", ["user_id", "role_id"], :name => "index_refinery_roles_users_on_user_id_and_role_id"
-
-  create_table "refinery_sermons", :force => true do |t|
-    t.datetime "date"
-    t.string   "speaker"
-    t.boolean  "service"
-    t.string   "title"
-    t.integer  "position"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "refinery_staff_categories", :force => true do |t|
     t.string   "name"
