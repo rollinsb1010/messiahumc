48403fbada5be4f3ea5ff7117b4afa3fa5a78c09
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-03-16 13:58:13 -0400

    Some refactoring for the worship view.

--- app/controllers/worship_controller.rb
+++ app/controllers/worship_controller.rb
@@ -1,4 +1,12 @@
 class WorshipController < ApplicationController
+  before_filter :find_page
+
   def index
+    present(@page)
+  end
+
+  protected
+  def find_page
+    @page = ::Refinery::Page.where(menu_match: '^/worship$').first
   end
 end
