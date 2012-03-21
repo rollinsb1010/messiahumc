489683572c15a93e7a05b13bb0a5ea824c1e9d57
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-03-21 15:55:22 -0400

    Fixed relationships between ministry and ministry_category.

--- vendor/extensions/ministries/app/models/refinery/ministries/ministry.rb
+++ vendor/extensions/ministries/app/models/refinery/ministries/ministry.rb
@@ -6,11 +6,14 @@ module Refinery
 
       self.table_name = 'refinery_ministries'
 
+      default_scope order: 'position ASC'
+
       acts_as_indexed fields: [:name, :description]
 
       validates :name, presence: true, uniqueness: true
 
       belongs_to :logo, class_name: '::Refinery::Image'
+      belongs_to :ministry_category, foreign_key: 'ministry_category_id', class_name: '::Refinery::Ministries::MinistryCategory'
 
     end
   end
