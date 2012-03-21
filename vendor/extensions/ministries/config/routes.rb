be3d7b63db5d42b6ddc2bf147a9a8fb6cfe7f9e0
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-03-21 15:23:34 -0400

    Adding link to manage categories and routes

--- vendor/extensions/ministries/config/routes.rb
+++ vendor/extensions/ministries/config/routes.rb
@@ -13,6 +13,11 @@ Refinery::Core::Engine.routes.append do
           post :update_positions
         end
       end
+      resources :ministry_categories, :except => :show do
+        collection do
+          post :update_positions
+        end
+      end
     end
   end
 
