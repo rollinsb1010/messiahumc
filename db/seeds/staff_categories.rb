8e0ae1ac89bd86c80ba883cd2e9e3287e09c04a7
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-03-21 16:26:08 -0400

    Added the ministry categories seed.

--- db/seeds/staff_categories.rb
+++ db/seeds/staff_categories.rb
@@ -4,6 +4,6 @@ if Refinery::StaffMembers::StaffCategory.all.empty?
   categories = ['Building Staff', 'Youth and Children', 'Music Staff', 'Our Day Preschool', 'Lay Leaders']
 
   categories.each_with_index do |c, index|
-    ::Refinery::StaffMembers::StaffCategory.create(name: c, index_placement: cycle('left', 'right'), position: index )
+    ::Refinery::StaffMembers::StaffCategory.create(name: c, index_placement: cycle('left', 'right'), position: index)
   end
 end
