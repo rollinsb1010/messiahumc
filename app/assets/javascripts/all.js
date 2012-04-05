7b25889762743f565de87f455e5c13c25bf72d02
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-04-04 21:48:12 -0400

    Added the home page slider.

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -1,6 +1,5 @@
 $(function() {
-    setInterval( "slideSwitch()", 5000 );
-
+    $('#slider').ramblingSlider({effect: 'fade'});
     var $sections = $('#sections li');
     var $down_menu_links = $('.down_menu a');
     var $up_arrow = $('.up_arrow');
@@ -22,20 +21,3 @@ $(function() {
 
     $down_menu_links.first().click();
 });
-
-function slideSwitch() {
-    var $active = $('#slideshow IMG.active');
-
-    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
-
-    var $next =  $active.next().length ? $active.next()
-        : $('#slideshow IMG:first');
-
-    $active.addClass('last-active');
-
-    $next.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            $active.removeClass('active last-active');
-        });
-}
