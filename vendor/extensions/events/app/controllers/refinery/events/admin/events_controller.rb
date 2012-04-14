ed5da854524e76b5e7a10ac5d4692c65ad7fb6c5
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-04-14 01:34:59 -0400

    Adding validators for events

--- vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
+++ vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
@@ -2,9 +2,13 @@ module Refinery
   module Events
     module Admin
       class EventsController < ::Refinery::AdminController
+        before_filter :get_ministries
 
-        crudify :'refinery/events/event', :xhr_paging => true
+        crudify :'refinery/events/event', xhr_paging: true
 
+        def get_ministries
+          @ministries = ::Refinery::Ministries::Ministry.all
+        end
       end
     end
   end
--- vendor/extensions/events/app/models/refinery/events/event.rb
+++ vendor/extensions/events/app/models/refinery/events/event.rb
@@ -2,12 +2,18 @@ module Refinery
   module Events
     class Event < Refinery::Core::BaseModel
       self.table_name = 'refinery_events'
+
       extend FriendlyId
       friendly_id :title, use: :slugged
 
       acts_as_indexed fields: [:title, :repetition, :location, :short_description, :long_description, :contact_name, :contact_email, :contact_phone, :notes]
 
       validates :title, presence: true, uniqueness: true
+      validates :date, presence: true
+      validates :repetition, presence: true, inclusion: {in: %w(never weekly monthly)}
+      validates :short_description, presence: true
+      validates :long_description, presence: true
+      validates :contact_email, email: true
 
       belongs_to :image, class_name: '::Refinery::Image'
 
