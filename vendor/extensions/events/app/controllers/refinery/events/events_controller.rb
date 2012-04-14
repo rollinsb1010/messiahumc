1d964222e8fbafba4aaa7637ac30cc920847777b
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-04-14 19:42:09 -0400

    Added the left sidebar to the events.

--- vendor/extensions/events/app/controllers/refinery/events/events_controller.rb
+++ vendor/extensions/events/app/controllers/refinery/events/events_controller.rb
@@ -1,34 +1,34 @@
 module Refinery
   module Events
     class EventsController < ::ApplicationController
-
-      before_filter :find_all_events
-      before_filter :find_page
+      before_filter :find_all_events, :find_page, :set_left_sidebar
 
       def index
-        # you can use meta fields from your model instead (e.g. browser_title)
-        # by swapping @page for @event in the line below:
         present(@page)
       end
 
       def show
         @event = Event.find(params[:id])
-
-        # you can use meta fields from your model instead (e.g. browser_title)
-        # by swapping @page for @event in the line below:
         present(@page)
       end
 
-    protected
-
+      protected
       def find_all_events
         @events = Event.order('position ASC')
       end
 
       def find_page
-        @page = ::Refinery::Page.where(:link_url => "/events").first
+        @page = ::Refinery::Page.where(link_url: '/events').first
       end
 
+      def set_left_sidebar
+        left_sidebar = show_left_sidebar 'Events'
+
+        left_sidebar.add_item 'Upcoming Events', '#'
+        left_sidebar.add_item 'Calendar', refinery.events_events_path
+        left_sidebar.add_item 'Weekly Messenger', '#'
+        left_sidebar.add_item 'Monthly Messenger', '#'
+      end
     end
   end
 end
