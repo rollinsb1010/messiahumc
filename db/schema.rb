7632027ce895a55c6394c56c03c75abb84973706
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-04-14 00:32:37 -0400

    Adding FriendlyId to Events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120414035952) do
+ActiveRecord::Schema.define(:version => 20120414042807) do
 
   create_table "refinery_copywriting_phrase_translations", :force => true do |t|
     t.integer  "refinery_copywriting_phrase_id"
@@ -72,7 +72,10 @@ ActiveRecord::Schema.define(:version => 20120414035952) do
     t.integer  "position"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+    t.string   "slug"
   end
+
+  add_index "refinery_events", ["slug"], :name => "index_refinery_events_on_slug", :unique => true
 
   create_table "refinery_image_page_translations", :force => true do |t|
     t.integer  "refinery_image_page_id"
--- vendor/extensions/events/app/models/refinery/events/event.rb
+++ vendor/extensions/events/app/models/refinery/events/event.rb
@@ -2,6 +2,8 @@ module Refinery
   module Events
     class Event < Refinery::Core::BaseModel
       self.table_name = 'refinery_events'
+      extend FriendlyId
+      friendly_id :title, use: :slugged
 
       acts_as_indexed fields: [:title, :repetition, :location, :short_description, :long_description, :contact_name, :contact_email, :contact_phone, :notes]
 
