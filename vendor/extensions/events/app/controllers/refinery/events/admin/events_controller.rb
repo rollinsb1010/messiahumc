b1295c75bea2e5099eb4dcebd3f1d4fb77b5a012
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-04-14 21:16:21 -0400

    Workaround for what seems to be a rails bug, time only fields aren't be nullable

--- vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
+++ vendor/extensions/events/app/controllers/refinery/events/admin/events_controller.rb
@@ -3,11 +3,25 @@ module Refinery
     module Admin
       class EventsController < ::Refinery::AdminController
         before_filter :get_ministries
+        before_filter :clean_nullable_time_fields, only: [:update, :create]
 
         crudify :'refinery/events/event', xhr_paging: true
 
         def get_ministries
           @ministries = ::Refinery::Ministries::Ministry.all
+        end
+
+        def clean_nullable_time_fields
+          event = params[:event]
+
+          nullable_fields = %w(start_time end_time)
+
+          nullable_fields.each do |field|
+            if event["#{field}(4i)"].empty? or event["#{field}(5i)"].empty?
+              (1..3).each { |index| event["#{field}(#{index}i)"] = ''}
+            end
+          end
+
         end
       end
     end
