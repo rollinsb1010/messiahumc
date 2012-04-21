6982570766db6a542b669bd5c28ff0592ee4c643
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-04-20 21:44:23 -0400

    Showing styled individual event data on calendar.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,21 +1,25 @@
 module EventsHelper
-
-  def get_time_info
+  def get_time_info(event = nil)
+    event = (event or @event)
     format = '%I:%M%P'
 
-    unless @event.start_time.nil?
-      time = @event.start_time.strftime(format)
+    unless event.start_time.nil?
+      time = event.start_time.strftime(format)
 
-      time + '-' + @event.end_time.strftime(format) unless @event.end_time.nil?
+      time + '-' + event.end_time.strftime(format) unless event.end_time.nil?
     end
   end
 
-  def get_contact_info
-    unless @event.contact_email.blank?
-      link = link_to (@event.contact_name or @event.contact_email), "mailto: #{@event.contact_email}"
+  def get_contact_info(event = nil)
+    event = (event or @event)
+    unless event.contact_email.blank?
+      link = link_to (event.contact_name or event.contact_email), "mailto: #{event.contact_email}"
     end
 
-    link + ", #{@event.contact_phone}" unless @event.contact_phone.blank?
+    link + ", #{event.contact_phone}" unless event.contact_phone.blank?
   end
 
+  def format_event_time(time)
+    time.present? ? time.strftime('%l:%M%P').gsub(/m/, '') : ''
+  end
 end
--- vendor/extensions/events/app/helpers/events_helper.rb
+++
@@ -1,5 +0,0 @@
-module EventsHelper
-  def format_event_time(time)
-    time.present? ? time.strftime('%l:%M%P').gsub(/m/, '') : ''
-  end
-end
