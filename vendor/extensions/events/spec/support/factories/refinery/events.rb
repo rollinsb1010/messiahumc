44839180c657faa4b9c14a11cea6d74dfec5adb7
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-04-24 00:40:28 -0400

    Added the monthly and weekly events factories.

--- vendor/extensions/events/spec/support/factories/refinery/events.rb
+++ vendor/extensions/events/spec/support/factories/refinery/events.rb
@@ -1,10 +1,18 @@
 FactoryGirl.define do
   factory :event, class: Refinery::Events::Event do
     sequence(:title) { |n| "Event #{n}" }
-    date Time.now
+    date Time.now.to_date
     repeats 'never'
     sequence(:short_description) { |n| "Short description for #{n}" }
     sequence(:long_description) { |n| "Long description for #{n}" }
     sequence(:contact_email) { |n| "event_contact#{n}@email.com" }
+  end
+
+  factory :weekly_event, parent: :event do
+    repeats 'weekly'
+  end
+
+  factory :monthly_event, parent: :event do
+    repeats 'monthly'
   end
 end
