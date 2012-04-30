87aa8307a2c24bee738c3412716b806ec3cc617e
Edgar Gonzalez <edggonzalezg@gmail.com>
2012-04-29 22:59:05 -0400

    Fixed broken style for search many results.

--- vendor/extensions/ministries/app/models/refinery/ministries/ministry.rb
+++ vendor/extensions/ministries/app/models/refinery/ministries/ministry.rb
@@ -22,6 +22,10 @@ module Refinery
         ::Refinery::Core::Engine.routes.url_helpers.ministries_ministry_path(self)
       end
 
+      def title
+        name
+      end
+
       def summary
         description
       end
