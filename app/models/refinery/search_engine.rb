3511857a929561954d3a31c5b8f5cf3a7574da84
Lilibeth De La Cruz <lili@ramblinglabs.com>
2012-04-30 01:41:11 -0400

    Overwrote search controller to handle pagination there

--- app/models/refinery/search_engine.rb
+++ app/models/refinery/search_engine.rb
@@ -1,16 +1,8 @@
 module Refinery
   class SearchEngine
 
-    # How many results should we show per page
-    RESULTS_LIMIT = 10
-
-    # Perform search over the specified models
-    def self.search(query, page = 1)
-      results = []
-
-      results << CombinedSearchItem.limit(RESULTS_LIMIT).with_query(query) if query.present?
-
-      results.flatten[0..(RESULTS_LIMIT - 1)]
+    def self.search(query)
+      CombinedSearchItem.with_query(query)
     end
 
   end
