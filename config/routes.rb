0529b89fa20ac7f85384bf6589056adf3343bbe8
Mike Doel <mike@mikedoel.com>
2012-08-26 16:19:08 -0400

    i dont understand the pages controller apparently

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ MessiahUmc::Application.routes.draw do
   get 'worship-page/:id', to: 'worship#show'
   get 'youth', to: 'youth#index'
   get 'youth/:id', to: 'youth#show'
+  get 'youth-page/:id', to: 'youth#show'
 
   mount Refinery::Core::Engine, at: '/'
 end
