5538d07b44ff3195b0e544a2332ccb116fe7bf33
Mike Doel <mike@mikedoel.com>
2012-09-27 10:48:50 -0400

    another try at inline viewing of attachments

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -45,4 +45,5 @@ Refinery::Core.configure do |config|
 
   # Register extra stylesheet for backend (optional options)
   config.register_stylesheet "custom_admin", :media => 'screen'
+  config.content_disposition = :inline
 end
