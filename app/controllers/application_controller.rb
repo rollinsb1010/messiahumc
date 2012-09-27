4038321b69ee4ac55450c3de6a397b13f7d1be90
Mike Doel <mike@mikedoel.com>
2012-09-27 17:19:18 -0400

    set inline for downloads in the application controller - yuck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_pagination_defaults
+  before_filter :set_pagination_defaults, :inline_file_downloads
   protect_from_forgery
   layout 'general'
 
   def set_pagination_defaults
     params[:page] ||= 1
     params[:per_page] ||= 10
+  end
+
+  def inline_file_downloads
+    # Force Dragonfly to view files attached to resources inline in the browser
+    ::Dragonfly[:refinery_resources].configure do |c|
+      c.content_disposition = :inline
+    end
   end
 end
