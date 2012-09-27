dfb7f640305e221869a8da8ab981daf91177486b
Mike Doel <mike@mikedoel.com>
2012-09-27 15:40:33 -0400

    another attempt to go inline

--- config/initializers/refinery/resources.rb
+++ config/initializers/refinery/resources.rb
@@ -25,7 +25,8 @@ Refinery::Resources.configure do |config|
 
 end
 
-dragonfly_app = Dragonfly[:refinery_resources]
+# Force Dragonfly to view files attached to resources inline in the browser
+dragonfly_app = ::Dragonfly[:refinery_resources]
 dragonfly_app.configure do |config|
   config.content_disposition = :inline
 end
