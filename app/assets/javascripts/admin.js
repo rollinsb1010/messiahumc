41fe73691ef130377ffd877b92ffd51580f2fef6
Mike Doel <mike@mikedoel.com>
2012-11-11 16:56:31 -0500

    ability to add new slot types

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -5,3 +5,15 @@
 var custom_wymeditor_boot_options = {
 
 };
+
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
+}
+
+function add_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
+  $(link).parent().before(content.replace(regexp, new_id));
+}
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,18 @@ module ApplicationHelper
   def readable_date(date)
     date.strftime('%B %d, %Y')
   end
+
+
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
+  
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
+
 end
